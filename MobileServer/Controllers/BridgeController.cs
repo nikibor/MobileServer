f040a84ba992da1b62eceb2034db93285a0df4d9
nikibor <nborgolov96@gmail.com>
2017-05-24 14:06:39 +0400

    1. Релиз базовой версии

--- MobileServer/Controllers/BridgeController.cs
+++ MobileServer/Controllers/BridgeController.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using MobileServer.Core;
 
 namespace MobileServer.Controllers
 {
     public class BridgeController : ApiController
     {
         
+        public string Get(long id)
+        {
+            var s = NeoTool.TakeFriends(148266446);
+            return "";
+        }
     }
 }
